f10ae8b4a2ff2277440808575d12948067e2d0b0
vthwang <vincent@fishboneapps.com>
2024-07-14 15:54:30 -0700

    feat: practical loading
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script>
 	import Test from '$lib/Test.svelte';
 	import '$db/start';
-	export let data
+	export let data;
+
+	$: ({ latest_episode, all_episodes } = data);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<h1>{data.latest_episode.title}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/about">About US</a>
+<h3>{latest_episode.title}</h3>
+<nav>
+	<ul>
+		{#each all_episodes as episode}
+			<li>
+				<a href="">{episode.title}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
 
 <Test />
