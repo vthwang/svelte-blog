8b30a666eabd00d4dd4800d01b1f2355a9a90445
vthwang <vincent@fishboneapps.com>
2024-07-14 16:31:33 -0700

    feat: loading in layouts
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,34 @@
 <script>
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
-
+	import Episodes from './Episodes.svelte';
 	import './styles.css';
+
+	export let data;
+	$: ({ all_episodes } = data);
 </script>
 
 <Header />
 
 <main>
-	<slot />
+	<div class="main">
+		<slot />
+	</div>
+	<aside>
+		<Episodes episodes={all_episodes} />
+	</aside>
 </main>
 
 <Footer />
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 300px 1fr;
+		gap: 20px;
+	}
+
+	aside {
+		order: -1;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,11 @@
 	import '$db/start';
 	export let data;
 
-	$: ({ latest_episode, all_episodes } = data);
+	$: ({ latest_episode } = data);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
 <h3>{latest_episode.title}</h3>
-<nav>
-	<ul>
-		{#each all_episodes as episode}
-			<li>
-				<a href={`/show/${episode.number}`}>{episode.title}</a>
-			</li>
-		{/each}
-	</ul>
-</nav>
 
 <Test />
