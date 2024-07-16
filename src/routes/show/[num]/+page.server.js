eb6abe7214071b1a43eb1e96a2d68a2b7d2bbf21
vthwang <vincent@fishboneapps.com>
2024-07-16 13:48:54 -0700

    feat: throwing errors
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/show/[num]/+page.server.js
+++ src/routes/show/[num]/+page.server.js
@@ -1,6 +1,14 @@
+import { error } from '@sveltejs/kit';
+
 export async function load({ fetch, params, setHeaders, locals }) {
 	const res = await fetch(`https://syntax.fm/api/shows/${params.num}`);
 	const data = await res.json();
+
+	if (data.message) {
+		throw error(404, {
+			message: data.message
+		});
+	}
 
 	setHeaders({
 		'Cache-Control': 'max-age=60'
