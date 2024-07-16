535e3b08a45a58be2df02002eceaef23b585d003
vthwang <vincent@fishboneapps.com>
2024-07-16 16:00:14 -0700

    feat: forms and form actions
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,4 @@
 <footer>
+	<a href="/contact">Contact</a>
 	<p>&copy; 2024 Level Up Tutorials</p>
 </footer>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,25 @@
+<form method="POST">
+	<label for="name">
+		Name: <input type="text" name="name" id="name" required />
+	</label>
+	<label for="email">
+		Email: <input type="email" name="email" id="email" required />
+	</label>
+	<label for="message">
+		Message: <textarea name="message" id="message" required />
+	</label>
+	<button type="submit"> Send Email </button>
+</form>
+
+<style>
+	form {
+		padding: 20px;
+		display: flex;
+		gap: 20px;
+		flex-direction: column;
+	}
+
+	label {
+		display: block;
+	}
+</style>
