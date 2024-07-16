02dbca70249c1ef56cd8ae432f978a4fe0f21d12
vthwang <vincent@fishboneapps.com>
2024-07-16 11:29:14 -0700

    feat: setHeaders and Caching
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/show/[num]/+page.server.js
+++ src/routes/show/[num]/+page.server.js
@@ -1,6 +1,10 @@
-export async function load({ fetch, params }) {
+export async function load({ fetch, params, setHeaders }) {
 	const res = await fetch(`https://syntax.fm/api/shows/${params.num}`);
 	const data = await res.json();
+
+	setHeaders({
+		'Cache-Control': 'max-age=60'
+	});
 
 	return {
 		episode: data
