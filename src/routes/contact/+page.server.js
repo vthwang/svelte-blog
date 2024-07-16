6eb5cd559873e5ec34041454143743d01e7da013
vthwang <vincent@fishboneapps.com>
2024-07-16 16:24:17 -0700

    feat: failing forms and the form prop
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,5 +1,13 @@
+import { fail } from '@sveltejs/kit';
+
 export const actions = {
-	default: async ({ request }) => {
+	default: async ({ locals, request }) => {
+		if (!locals?.user?.roles?.includes('admin')) {
+			return fail(401, {
+				error_message: 'Unauthorized'
+			});
+		}
+
 		const data = await request.formData();
 		const name = data.get('name');
 		const email = data.get('email');
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,19 +1,30 @@
 <script>
 	import { enhance } from '$app/forms';
+
+	export let form;
+	$: console.log('form', form);
 </script>
 
-<form use:enhance method="POST">
-	<label for="name">
-		Name: <input type="text" name="name" id="name" required />
-	</label>
-	<label for="email">
-		Email: <input type="email" name="email" id="email" required />
-	</label>
-	<label for="message">
-		Message: <textarea name="message" id="message" required />
-	</label>
-	<button type="submit"> Send Email </button>
-</form>
+{#if form?.error_message}
+	<p>{form.error_message}</p>
+{/if}
+
+{#if form?.message}
+	<p>{form.message}</p>
+{:else}
+	<form use:enhance method="POST">
+		<label for="name">
+			Name: <input type="text" name="name" id="name" required />
+		</label>
+		<label for="email">
+			Email: <input type="email" name="email" id="email" required />
+		</label>
+		<label for="message">
+			Message: <textarea name="message" id="message" required />
+		</label>
+		<button type="submit"> Send Email </button>
+	</form>
+{/if}
 
 <style>
 	form {
